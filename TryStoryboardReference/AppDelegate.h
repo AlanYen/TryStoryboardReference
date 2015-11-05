1af4adad7213c5c44041e692694fcc99e8a92e63
AlanYen <alanyen.mobile@gmail.com>
2015-11-05 15:55:57 +0800

    Initial Commit

---
+++ TryStoryboardReference/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TryStoryboardReference
+//
+//  Created by AlanYen on 2015/11/5.
+//  Copyright © 2015年 17Life. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TryStoryboardReference/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TryStoryboardReference
+//
+//  Created by AlanYen on 2015/11/5.
+//  Copyright © 2015年 17Life. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
